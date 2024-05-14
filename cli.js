cd3b6e9d881596abb42c6126a9d483f9a7cd2caf
CJ <csh.joo77@gmail.com>
2024-05-13 21:45:36 -0400

    changed the cli js to ensure update department, role, employee, employee role feature is added and accesible with responses as input. changed the dbhelp.js so it can maintain the changes made to *departments employees* for example

--- cli.js
+++ cli.js
@@ -1,7 +1,5 @@
 const inquirer = require('inquirer');
-
 const dbhelp = require('./helpers/dbhelp');
-
 
 async function mainMenu() {
   const { choice } = await inquirer.prompt([
@@ -13,7 +11,10 @@ async function mainMenu() {
         'View all departments',
         'View all roles',
         'View all employees',
-       
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role'
       ],
     },
   ]);
@@ -28,10 +29,20 @@ async function mainMenu() {
     case 'View all employees':
       viewEmployees();
       break;
-    
+    case 'Add a department':
+      addDepartment();
+      break;
+    case 'Add a role':
+      addRole();
+      break;
+    case 'Add an employee':
+      addEmployee();
+      break;
+    case 'Update an employee role':
+      updateEmployeeRole();
+      break;
   }
 }
-
 
 async function viewDepartments() {
   const departments = await dbhelp.getAllDepartments();
@@ -39,17 +50,129 @@ async function viewDepartments() {
   mainMenu();
 }
 
-
 async function viewRoles() {
   const roles = await dbhelp.getAllRoles();
   console.table(roles);
   mainMenu();
 }
 
-
 async function viewEmployees() {
   const employees = await dbhelp.getAllEmployees();
   console.table(employees);
+  mainMenu();
+}
+
+async function addDepartment() {
+  const { departmentName } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: 'Enter the name of the department:',
+    },
+  ]);
+
+  await dbhelp.addDepartment(departmentName);
+  console.log('Department added successfully!');
+  mainMenu();
+}
+
+async function addRole() {
+  const departments = await dbhelp.getAllDepartments();
+  const departmentChoices = departments.map(department => ({
+    name: department.name,
+    value: department.id
+  }));
+
+  const { title, salary, departmentId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of the role:',
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: 'Enter the salary for the role:',
+    },
+    {
+      type: 'list',
+      name: 'departmentId',
+      message: 'Select the department for the role:',
+      choices: departmentChoices
+    }
+  ]);
+
+  await dbhelp.addRole(title, salary, departmentId);
+  console.log('Role added successfully!');
+  mainMenu();
+}
+
+async function addEmployee() {
+  const roles = await dbhelp.getAllRoles();
+  const roleChoices = roles.map(role => ({
+    name: role.title,
+    value: role.id
+  }));
+
+  const { firstName, lastName, roleId, managerId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter the first name of the employee:',
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Enter the last name of the employee:',
+    },
+    {
+      type: 'list',
+      name: 'roleId',
+      message: 'Select the role for the employee:',
+      choices: roleChoices
+    },
+    {
+      type: 'input',
+      name: 'managerId',
+      message: 'Enter the manager ID for the employee (if applicable):',
+    }
+  ]);
+
+  await dbhelp.addEmployee(firstName, lastName, roleId, managerId);
+  console.log('Employee added successfully!');
+  mainMenu();
+}
+
+async function updateEmployeeRole() {
+  const employees = await dbhelp.getAllEmployees();
+  const employeeChoices = employees.map(employee => ({
+    name: `${employee.first_name} ${employee.last_name}`,
+    value: employee.id
+  }));
+
+  const roles = await dbhelp.getAllRoles();
+  const roleChoices = roles.map(role => ({
+    name: role.title,
+    value: role.id
+  }));
+
+  const { employeeId, roleId } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'employeeId',
+      message: 'Select the employee whose role you want to update:',
+      choices: employeeChoices
+    },
+    {
+      type: 'list',
+      name: 'roleId',
+      message: 'Select the new role for the employee:',
+      choices: roleChoices
+    }
+  ]);
+
+  await dbhelp.updateEmployeeRole(employeeId, roleId);
+  console.log('Employee role updated successfully!');
   mainMenu();
 }
 
--- helpers/dbhelp.js
+++ helpers/dbhelp.js
@@ -1,28 +1,51 @@
 const pool = require('../db/db');
 
 const dbhelp = {
-
   async getAllDepartments() {
     const query = 'SELECT * FROM department';
     const { rows } = await pool.query(query);
     return rows;
   },
   
- 
   async getAllRoles() {
     const query = 'SELECT * FROM role';
     const { rows } = await pool.query(query);
     return rows;
   },
   
-
   async getAllEmployees() {
     const query = 'SELECT * FROM employee';
     const { rows } = await pool.query(query);
     return rows;
   },
-  
 
+  async addDepartment(departmentName) {
+    const query = 'INSERT INTO department (name) VALUES ($1) RETURNING *';
+    const values = [departmentName];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  },
+
+  async addRole(title, salary, departmentId) {
+    const query = 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *';
+    const values = [title, salary, departmentId];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  },
+
+  async addEmployee(firstName, lastName, roleId, managerId) {
+    const query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *';
+    const values = [firstName, lastName, roleId, managerId];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  },
+
+  async updateEmployeeRole(employeeId, roleId) {
+    const query = 'UPDATE employee SET role_id = $1 WHERE id = $2 RETURNING *';
+    const values = [roleId, employeeId];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+  }
 };
 
 module.exports = dbhelp;
